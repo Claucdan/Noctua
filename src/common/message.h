3ab9d1d0322ecf2ce0330da180de3b7e8451ecb0
hhhh hgfh <dania.lukin@gmail.com>
2026-02-18 23:55:30 +0300

    feat(src): add message class to src

---
+++ src/common/message.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <memory>
+#include <string_view>
+
+namespace noctua {
+
+class message_t final {
+private:
+  class deleter_t {
+    void operator()(message_t* data) {
+      std::destroy_at(data);
+      free(data);
+    }
+  };
+
+public:
+  using message_ptr_t = std::unique_ptr<message_t, deleter_t>;
+
+  message_t() = delete;
+
+  message_t(message_t& other) = delete;
+  message_t(message_t&& other) = delete;
+
+  explicit message_t(std::string_view msg)
+      : data_len_(msg.size()) {
+    std::ranges::copy(msg, data_);
+  }
+
+  message_t& operator=(message_t& other) = delete;
+  message_t& operator=(message_t&& other) = delete;
+
+  ~message_t() = default;
+
+  [[nodiscard]] std::string_view get_data() const noexcept {
+    return {data_, data_len_};
+  }
+
+  [[nodiscard]] size_t size() const noexcept {
+    return data_len_;
+  }
+
+  static message_ptr_t create_message(std::string_view msg) {
+    auto raw_data = static_cast<message_t*>(malloc(sizeof(message_t) + msg.size()));
+    std::construct_at(raw_data, msg);
+    return message_ptr_t{raw_data};
+  }
+
+private:
+  uint32_t data_len_;
+  char data_[];
+};
+
+} // namespace noctua
