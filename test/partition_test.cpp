19d80ddde231ef39ec4ead1c4e1512acd293b1c4
hhhh hgfh <dania.lukin@gmail.com>
2026-02-19 19:17:06 +0300

    tests(partition): add tests for partititon

--- test/partition_test.cpp
+++ test/partition_test.cpp
@@ -30,4 +30,86 @@ TEST(PartitionTest, SingleReaderCoroutine) {
   boost::cobalt::run(reader_task());
 }
 
+TEST(PartitionTest, PushAndFront) {
+  partition_t partition;
+
+  partition.push(message_t::create_message("first"));
+  partition.push(message_t::create_message("second"));
+  partition.push(message_t::create_message("third"));
+
+  EXPECT_EQ("first", partition.front().get_data());
+}
+
+TEST(PartitionTest, PushAndBack) {
+  partition_t partition;
+
+  partition.push(message_t::create_message("first"));
+  partition.push(message_t::create_message("second"));
+  partition.push(message_t::create_message("third"));
+
+  EXPECT_EQ("third", partition.back().get_data());
+}
+
+TEST(PartitionTest, PopRemovesFrontElement) {
+  partition_t partition;
+
+  partition.push(message_t::create_message("first"));
+  partition.push(message_t::create_message("second"));
+
+  EXPECT_EQ("first", partition.front().get_data());
+
+  partition.pop();
+
+  EXPECT_EQ("second", partition.front().get_data());
+}
+
+TEST(PartitionTest, MultiplePops) {
+  partition_t partition;
+
+  partition.push(message_t::create_message("first"));
+  partition.push(message_t::create_message("second"));
+  partition.push(message_t::create_message("third"));
+
+  partition.pop();
+  EXPECT_EQ("second", partition.front().get_data());
+
+  partition.pop();
+  EXPECT_EQ("third", partition.front().get_data());
+}
+
+TEST(PartitionTest, WriteLockCoroutine) {
+  partition_t partition;
+  message_ptr_t msg = message_t::create_message("data");
+
+  auto writer_task = [&]() -> boost::cobalt::task<void> {
+    auto lk = co_await partition.write_lock();
+    partition.push(std::move(msg));
+    EXPECT_EQ("data", partition.front().get_data());
+    co_return;
+  };
+
+  boost::cobalt::run(writer_task());
+}
+
+TEST(PartitionTest, ReadWriteWithLocks) {
+  partition_t partition;
+
+  auto writer_task = [&]() -> boost::cobalt::task<void> {
+    auto lk = co_await partition.write_lock();
+    partition.push(message_t::create_message("first"));
+    partition.push(message_t::create_message("second"));
+    co_return;
+  };
+
+  auto reader_task = [&]() -> boost::cobalt::task<void> {
+    auto lk = co_await partition.read_lock();
+    EXPECT_EQ("first", partition.front().get_data());
+    EXPECT_EQ("second", partition.back().get_data());
+    co_return;
+  };
+
+  boost::cobalt::run(writer_task());
+  boost::cobalt::run(reader_task());
+}
+
 } // namespace noctua::partitions
