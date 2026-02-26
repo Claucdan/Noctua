6bd5f92942491720e8089131a3a6ce36a69cf331
hhhh hgfh <dania.lukin@gmail.com>
2026-02-26 20:40:46 +0300

    refactor: change request to new api

--- src/common/request.h
+++ src/common/request.h
@@ -6,20 +6,38 @@ namespace noctua::common {
 
 #pragma pack(push, 4)
 
-class request_body_t {
+class request_t {
 public:
-  request_body_t() = delete;
-  request_body_t(const request_body_t&) = delete;
-  request_body_t(request_body_t&&) = delete;
+  request_t() = delete;
+  request_t(const request_t&) = delete;
+  request_t(request_t&&) = delete;
 
-  request_body_t& operator=(const request_body_t&) = delete;
-  request_body_t& operator=(request_body_t&&) = delete;
+  request_t& operator=(const request_t&) = delete;
+  request_t& operator=(request_t&&) = delete;
+
+  ~request_t() = default;
+
+  [[nodiscard]] uint16_t topic_name_len() const noexcept {
+    return topic_name_len_;
+  }
+
+  [[nodiscard]] uint16_t partition_id() const noexcept {
+    return partition_id_;
+  }
+
+  [[nodiscard]] std::string_view topic_name() const noexcept {
+    return {reinterpret_cast<const char*>(data_), topic_name_len_};
+  }
+
+  [[nodiscard]] std::string_view message_view() const noexcept {
+    return {reinterpret_cast<const char*>(data_) + topic_name_len_, message_len_};
+  }
 
 private:
-  uint16_t topic_name_len;
-  uint16_t partition_id;
-  uint16_t message_len;
-  std::byte data[];
+  uint16_t topic_name_len_;
+  uint16_t partition_id_;
+  uint16_t message_len_;
+  std::byte data_[];
 };
 
 #pragma pack(pop)
